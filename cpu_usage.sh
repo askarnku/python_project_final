43ed05c4124ab9f01f63ac0778b538894a644a94
Askar Mamytov <askar.mamytov201@gmail.com>
2024-07-25 00:33:32 -0500

    add cron job

--- cpu_usage.sh
+++ cpu_usage.sh
@@ -2,8 +2,6 @@
 
 source ~/.bashrc
 
-echo $(date)
-
 #cpu use threshold
 cpu_threshold="80"
 
@@ -14,11 +12,7 @@ mem_threshold="50"
 disk_threshold="90"
 
 #batch13 web hook
-slack_wh="$SLACK_WEBHOOK_URL_DEV"
-
-echo "$slack_wh"
-
-
+slack_wh="$SLACK_WEBHOOK_URL"
 
 
 declare -A nodes
@@ -26,18 +20,15 @@ declare -A nodes
 nodes[node1]='ec2-user@3.95.184.194'
 nodes[node2]='ec2-user@54.237.186.247'
 
-# Print all elements of the associative array
+
 for key in "${!nodes[@]}"; do
-    echo "Key: $key, Value: ${nodes[$key]}"
+
     child_server="${nodes[$key]}"
 
-    # Get CPU usage by using the top command and parse it
     cpu_usage=$(ssh $child_server "mpstat 1 1 | awk '/Average:/ {print 100 - \$12}'")
 
-    # Get memory usage
     memory_usage=$(ssh $child_server "free -m | awk '/Mem:/ {print \$3/\$2 * 100.0}'")
 
-    # Get disk usage
     disk_usage=$(ssh $child_server "df -h / | awk '/\// {print \$5}'")
     
     #stat print
