778bdcbeca2dcf9eeef5735a5fffcbc7afa39a74
Askar Mamytov <askar.mamytov201@gmail.com>
2024-07-24 23:44:21 -0500

    slack several lines of text

--- slack_api.sh
+++ slack_api.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-slack_wh='https://hooks.slack.com/services/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'
+slack_wh="$SLACK_WEBHOOK_URL_DEV"
 
 curl -X POST -H 'Content-type: application/json' --data '{"text":"Testing"}' $slack_wh
+
+
+# Example variables
+key="SomeKey"
+payload="This is some payload information"
+
+# Generate JSON payload with multiple lines using jq
+json_payload=$(jq -n --arg key "$key" --arg payload "$payload" \
+    '{text: ($key + "\n" + $payload)}')
+
+# Use curl to send the payload
+curl -X POST -H 'Content-type: application/json' --data "$json_payload" $slack_wh
+
+
+
