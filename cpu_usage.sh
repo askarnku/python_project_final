2245a946fcc61c882d4b8551cf590e0052d0ae19
Askar Mamytov <askar.mamytov201@gmail.com>
2024-07-24 21:08:29 -0500

    update nodes ips and loop

--- cpu_usage.sh
+++ cpu_usage.sh
@@ -9,6 +9,17 @@ nodes[node2]='ec2-user@54.237.186.247'
 # Print all elements of the associative array
 for key in "${!nodes[@]}"; do
     echo "Key: $key, Value: ${nodes[$key]}"
+    child_server="${nodes[$key]}"
+
+    # Get CPU usage by using the top command and parse it
+    cpu_usage=$(ssh $child_server "mpstat 1 1 | awk '/Average:/ {print 100 - \$12}'")
+
+    # Get memory usage
+    memory_usage=$(ssh $child_server "free -m | awk '/Mem:/ {print \$3/\$2 * 100.0}'")
+
+    # Get disk usage
+    disk_usage=$(ssh $child_server "df -h / | awk '/\// {print \$5}'")
+
 done
 
 exit 1
@@ -18,7 +29,7 @@ exit 1
 
 
 # Define the child server's SSH details
-child_server="ec2-user@3.95.184.194"
+
 
 # Get CPU usage by using the top command and parse it
 cpu_usage=$(ssh $child_server "mpstat 1 1 | awk '/Average:/ {print 100 - \$12}'")
